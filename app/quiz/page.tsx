4ff94a75b252b431d8c1f70a3662556b3620da7c
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2023-07-29 01:27:24 +0530

    feat:Start UI ready

---
+++ app/quiz/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="w-[320px] h-[640px] bg-violet-400 rounded-lg">
+      
+    </div>
+  );
+}
+
+export default page
