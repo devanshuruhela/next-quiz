18540d1354c7357da8c043036e6f880c9bfc8214
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2023-08-25 23:19:00 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
    <div className="w-[320px] h-[640px] bg-gradient-to-b from-white to-violet-400 rounded-lg flex flex-col justify-between items-center">
     <div className="flex justify-center gap-2 mt-5">
       <Image src="/logo.svg" height={30} width={30} alt="mainlogo"/>
-      <p className="text-[18px] text-neutral-800 font-[900]">Upraised</p>
+      <p className="text-[18px] text-neutral-800 font-[900]">Quiz</p>
     </div>
     <div className="h-[120px] w-[120px] bg-white flex flex-col justify-center items-center rounded-full shadow-md">
-      <p className="text-red-500 font-extrabold text-[25px]">Quiz</p>
+      <p className="text-red-500 font-extrabold text-[25px]">Welcome</p>
     </div>
 
     <button className="py-2 px-[100px] bg-red-500 text-white text-[18px] font-extrabold rounded-3xl mb-5 hover:bg-red-600" onClick={()=>router.push('/quiz')}>Start</button>
